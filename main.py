464bc2df97afc60ae790e2a11e0a05d6cf1a2b97
Alex <blyxyas@gmail.com>
2022-03-11 22:50:41 +0000

    rggfd

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 # We load the environment variables
 load_dotenv()
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
  
 DEFAULT_PREFIX = ">"
 
@@ -90,26 +90,33 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+
+  # We see if the last time someone used the bot is more than a day, we just clear the list (data is expensive) 
+  if time() - db[message.guild.id][0] >= 3600 * 24:
+    db[message.guild.id][1].clear()
+
+  list_users = db[message.guild.id][1]
 	# We check if the user is registered
-	if message.author.id not in db[message.guild.id]:
+	if message.author.id not in list_users:
 		# We register the user
-		db[message.guild.id].append(user(message.guild.id, message.author.id, 0, time.time()))
+		list_users.append(user(message.guild.id, message.author.id, 0, time.time()))
 		print(f"{message.author.name} has been registered")
 	else:
 		if any(x in message.content.lower() for x in ["cat", "kitty", "kitten", "kittycat", "kittens", "kittycats", "kitties"]):
 			for keyw in ["cat", "kitty", "kitten", "kittycat", "kittens", "kittycats", "kitties"]:
 				if keyw in message.content.lower():
 					cat_counter += message.content.lower().count(keyw)
-			# Now we update the user
-			db[message.guild.id][message.author.id].more_cats(cat_counter)
+			
+      # Now we update the user
+			list_users[message.author.id].more_cats(cat_counter)
 
-		if time.time() - db[message.guild.id][message.author.id].last_time > 60:
+		if time.time() - list_users[message.author.id].last_time > 60:
 
-			db[message.guild.id][message.author.id].reset_cats()
+			list_users[message.author.id].reset_cats()
 		
 		else:
-			if db[message.guild.id][message.author.id].cat_counter >= 3:
+			if list_users[message.author.id].cat_counter >= 3:
 				await reply(message)
-				db[message.guild.id][message.author.id].reset_cats()
+				list_users[message.author.id].reset_cats()
 
 bot.run(DISCORD_TOKEN)
