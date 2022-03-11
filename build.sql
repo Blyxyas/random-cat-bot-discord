f2f68bf766473d28fdf917430e4e3de2f8fcfeaf
Blyxyas <blyxyas@gmail.com>
2022-03-11 13:29:50 +0100

    now it kinda works

--- build.sql
+++ build.sql
@@ -1,11 +1,24 @@
+-- Create table `servers` with columns:
+-- `guild_id`: bigint unsigned not null primary key,
+-- `prefix`: varchar(5) not null default '!',
+-- users: List of users that have access to the bot.
+
 CREATE TABLE IF NOT EXISTS servers (
-	serverID INTEGER NOT NULL,
-	user PRIMARY KEY(serverID)
+  guild_id bigint unsigned not null primary key,
+  prefix varchar(5) not null default '!',
 );
 
--- Each server has a list of users
-CREATE TABLE IF NOT EXISTS user (
-	userid INTEGER NOT NULL PRIMARY KEY,
-	cat_counter INTEGER NOT NULL DEFAULT 0,
-	last_time INTEGER FOREIGN KEY (userid) REFERENCES servers(user)
+-- Create table `users` with columns:
+-- `user_id`: bigint unsigned not null primary key,
+-- `username`: varchar(32) not null,
+-- `discriminator`: varchar(4) not null,
+-- `cat_counter`: int unsigned not null default 0,
+-- `last_time`: timestamp not null,
+-- Table `users` is used to store information about users.
+
+CREATE TABLE IF NOT EXISTS users (
+  userid bigint unsigned not null primary key,
+  cat_counter int unsigned not null default 0,
+  last_time timestamp not null
 );
+
